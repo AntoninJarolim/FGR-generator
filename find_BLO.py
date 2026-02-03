77be7f59dcfc27b23d664f31681a1a06c7e6ee92
Antonín Jarolím <jarolim.antonin@seznam.cz>
2026-02-03 02:49:55 +0100

    Add 'find_all_paths' to generate string

--- find_BLO.py
+++ find_BLO.py
@@ -59,14 +59,10 @@ class TokenByteFinder:
             for t, t_id in self.vocab.items()
         }
 
-    def get_singe_token(self, char):
-        single_id = self.tokenizer.encode(char, add_special_tokens=False)
-        if len(single_id) == 1:
-            return single_id[0]
-        raise AssertionError("Char encoded is not a single token")
-
-
-
+    def get_single_token(self, char):
+        # [0] to remove batch dim
+        single_id = self.tokenizer.encode(char, add_special_tokens=False, return_tensors="pt")[0]
+        return single_id
 
     def find_prefix_overlaps(self, target_bytes):
         target_bytes = encode_str_utf8(target_bytes)
@@ -76,7 +72,7 @@ class TokenByteFinder:
             max_k = min(len(b), len(target_bytes) - 1)
             for k in range(1, max_k + 1):
                 if b[-k:] == target_bytes[:k]:
-                    prefix_overlaps[tok_id] = k
+                    prefix_overlaps[tok_id] = b
                     break
         return prefix_overlaps
 
@@ -84,8 +80,20 @@ class TokenByteFinder:
         target_bytes = encode_str_utf8(target_bytes)
 
         containment = {
-            tok_id: True for tok_id, b in self.token_bytes.items()
+            tok_id: b for tok_id, b in self.token_bytes.items()
             if target_bytes in b
+        }
+        return containment
+
+    def find_starting_with(self, target_bytes):
+        target_bytes = encode_str_utf8(target_bytes)
+        target_bytes_starts = [
+            target_bytes[:k] for k in range(1, len(target_bytes) + 1)
+        ]
+
+        containment = {
+            tok_id: b for tok_id, b in self.token_bytes.items()
+            if any(b.startswith(target_bytes_start) for target_bytes_start in target_bytes_starts)
         }
         return containment
 
@@ -93,9 +101,18 @@ class TokenByteFinder:
         target_bytes = encode_str_utf8(any_string)
         prefix_overlaps = self.find_prefix_overlaps(target_bytes)
         containment = self.find_containment_tokens(target_bytes)
-        
+
         # Merging keys from both dictionaries and removing duplicates
         generating_tokens = list(set(prefix_overlaps.keys()) | set(containment.keys()))
+        return generating_tokens
+
+    def get_generating_tokens_all(self, any_string):
+        target_bytes = encode_str_utf8(any_string)
+        prefix_overlaps = self.find_prefix_overlaps(target_bytes)
+        containment = self.find_containment_tokens(target_bytes)
+
+        # Merging keys from both dictionaries and removing duplicates
+        generating_tokens = {**prefix_overlaps, **containment}
         return generating_tokens
 
     def return_token_bytes(self, ids):
@@ -107,13 +124,50 @@ class TokenByteFinder:
 
         return None
 
+    def find_all_paths(self, target_bytes):
+        generating_tokens = self.get_generating_tokens_all(target_bytes)
+        path_list = [[(k, v)] for k, v in generating_tokens.items()]
+        max_size = 5
+        target_bytes_starts = [
+            target_bytes[:k] for k in range(1, len(target_bytes) + 1)
+        ]
+
+        for i in range(max_size):
+            longer_path_list = []
+            for path in path_list:
+                all_bytes = join_path_bytes(path)
+                if target_bytes in all_bytes:
+                    longer_path_list.append(path)
+                else:
+                    target_continuation = remove_overlap_prefix(target_bytes, all_bytes)
+                    next_steps = self.find_starting_with(target_continuation)
+                    for next_step in next_steps.items():
+                        new_path = path.copy()
+                        new_path.append(next_step)
+
+                        # check path validity
+                        new_path_bytes = join_path_bytes(new_path)
+                        valid = any(
+                            new_path_bytes.endswith(target_bytes_start) for target_bytes_start in target_bytes_starts)
+
+                        if valid:
+                            longer_path_list.append(new_path)
+
+            path_list = longer_path_list
+            if all(target_bytes in join_path_bytes(new_path)
+                   for new_path in longer_path_list):
+                break
+        return path_list
+
 
 def bytes_to_hex(b: bytes) -> str:
     return " ".join(f"0x{byte:02x}" for byte in b)
 
+
 def safe_utf8(b: bytes) -> str:
     """Best-effort UTF-8 decoding (shows  for invalid sequences)."""
     return b.decode("utf-8", errors="replace")
+
 
 def print_token_info(tok_id, oracle, k=None):
     b = oracle.token_bytes[tok_id]
@@ -125,6 +179,7 @@ def print_token_info(tok_id, oracle, k=None):
     if k is not None:
         print(f"Overlap size : {k} byte(s)")
         print(f"Overlap bytes: {bytes_to_hex(b[-k:])}")
+
 
 def print_results(oracle, TARGET_UTF):
     target_bytes = TARGET_UTF.encode("utf-8")
@@ -171,6 +226,17 @@ def print_results(oracle, TARGET_UTF):
             print_token_info(tok_id, oracle)
             print("-" * 60)
 
+def remove_overlap_prefix(target: bytes, previous: bytes) -> bytes:
+    max_k = min(len(target), len(previous))
+    for k in range(max_k, 0, -1):
+        if previous.endswith(target[:k]):
+            return target[k:]
+    return target
+
+def join_path_bytes(path):
+    return b"".join([v for _, v in path])
+
+
 if __name__ == "__main__":
     MODEL_NAME = "meta-llama/Meta-Llama-3-8B-Instruct"
     # Examples:
@@ -179,13 +245,39 @@ if __name__ == "__main__":
     #   "gpt2"
     #   "EleutherAI/gpt-neox-20b"
 
-    TARGET_UTF = "€" # "—"  "‡" try: "€", "—", "🙂", "\u0301"
+    TARGET_UTF = "‡"  # "—"  "‡" ,"€", "—", "🙂", "\u0301"
 
     oracle = TokenByteFinder(MODEL_NAME)
-    
-    # The get_generating_tokens method can be used to get the combined list of tokens
-    generating_tokens = oracle.get_generating_tokens(TARGET_UTF)
-    # print(f"Generating Tokens: {generating_tokens}")
 
-    # The printing of results is done by calling print_results
+    # The get_generating_tokens method can be used to get the combined list of tokens
+    TARGET_UTF_BYTES = encode_str_utf8(TARGET_UTF)
+
+
+    def print_paths(paths, target_bytes):
+        paths = sorted(paths, key=lambda x: len(x))
+        for i, path in enumerate(paths, 1):
+            print(f"Path {i}:")
+            for step_idx, (k, v) in enumerate(path, 1):
+                try:
+                    dec = v.decode('utf-8')
+                except UnicodeDecodeError:
+                    dec = 'invalid'
+                print(f"  {step_idx}. id={k}, bytes={v}, bytes={dec!r}")
+            path_bytes = join_path_bytes(path)
+            ids = list(k for k, _ in path)
+            ids_decoded = oracle.tokenizer.decode(ids)
+            ids_bytes = oracle.return_token_bytes(ids)
+            print(f"   path bytes={path_bytes} bytes_dec={path_bytes.decode('utf-8')!r}")
+            print(f"   path ids={ids} ids_dec={ids_decoded!r}")
+
+            assert b''.join(ids_bytes) == path_bytes
+            assert path_bytes.decode('utf-8') == ids_decoded
+            assert target_bytes in path_bytes, ""
+            print("-" * 40)
+
+
+    path_list = oracle.find_all_paths(TARGET_UTF_BYTES)
+    print_paths(path_list, TARGET_UTF_BYTES)
+
+    # The printing of results is done by calling print_results__repr__()
     print_results(oracle, TARGET_UTF)
